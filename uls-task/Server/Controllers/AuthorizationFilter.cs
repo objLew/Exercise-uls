beefc17c67c6ae5a302d9433d54a86f4b0a2b093
Lewis Lovette <lewisjalove@hotmail.com>
2024-04-22 17:33:09 +0100

    [➕ADD] AuthorizationFilter + main controller set-up

---
+++ uls-task/Server/Controllers/AuthorizationFilter.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace uls_task.Server.Controllers
+{
+    public class AuthorizationFilter : IAuthorizationFilter
+    {
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            // this could be more in depth but for simplicity we will just check for a hard-coded header
+
+            if (context.HttpContext.Request.Headers.ContainsKey("ShhhhThisIsASecret"))
+            {
+                string secret = context.HttpContext.Request.Headers["ShhhhThisIsASecret"];
+
+                if (string.IsNullOrEmpty(secret))
+                {
+                    // return unauthorized
+                    context.Result = new StatusCodeResult(403);
+
+                    return;
+                }
+            }
+
+            // continue to controller
+            return;
+
+        }
+
+
+    }
+}
--- uls-task/Server/Controllers/GeneralController.cs
+++ uls-task/Server/Controllers/GeneralController.cs
@@ -1,12 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GeneralLibrary;
+using Microsoft.AspNetCore.Mvc;
 
-namespace uls-task.Server.Controllers
+namespace uls_task.Server.Controllers
 {
+    /// <summary>
+    /// Define specific routing and apply authorization filter to ALL functions
+    /// </summary>
+    [ApiController]
+    [Route("api/calculator")]
+    [TypeFilter(typeof(AuthorizationFilter))]
     public class GeneralController : Controller
-{
-    public IActionResult Index()
     {
-        return View();
+        [HttpGet]
+        [Route("CalculateString")]
+        public IActionResult CalculateString(string CalculateThis)
+        {
+            try
+            {
+                bool isValidString = DecodeHelper.SanityCheckInput(CalculateThis);
+
+                if (!isValidString)
+                {
+                    return BadRequest("Input string is invalid");
+                }
+            }
+            catch 
+            {
+                return BadRequest("Input string is invalid");
+            }
+
+
+
+
+
+
+
+            return BadRequest();
+
+            return View();
+        }
     }
-}
 }
